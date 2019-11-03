bba5cfa60ef8d5671eb2115126dd6a7626712c72
Javier Rodríguez <jrolopez20@gmail.com>
2019-11-03 07:07:56 +0100

    Updates

---
+++ precache-manifest.351a8a1ae385acd7205a020897db2f33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4834fdc7567b268542b12fd91ccc0f86",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b9c4c132acd4580f2411",
+    "url": "/portfolio/static/css/main.bbecde6e.chunk.css"
+  },
+  {
+    "revision": "018a576ad37e5df9f72c",
+    "url": "/portfolio/static/js/2.333da9f7.chunk.js"
+  },
+  {
+    "revision": "b9c4c132acd4580f2411",
+    "url": "/portfolio/static/js/main.9ff72702.chunk.js"
+  },
+  {
+    "revision": "9cb5c3505ff2ca315d08",
+    "url": "/portfolio/static/js/runtime-main.ea8b6c93.js"
+  }
+]);
